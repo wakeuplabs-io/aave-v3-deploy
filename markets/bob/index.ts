df41ce0eee9c45ef113cbe36e4ad5e9c6930003e
juandahl <juandahl20@gmail.com>
2024-10-03 13:31:16 -0300

    feat(reserves): add aave asset and deployment

--- markets/bob/index.ts
+++ markets/bob/index.ts
@@ -1,6 +1,7 @@
 import { eBobNetwork, IAaveConfiguration } from "./../../helpers/types";
 import AaveMarket from "../aave";
 import {
+  strategyAAVE,
   strategyDAI,
   strategyUSDC,
   strategyWETH,
@@ -17,12 +18,14 @@ export const BobConfig: IAaveConfiguration = {
     DAI: strategyDAI,
     USDC: strategyUSDC,
     WETH: strategyWETH,
+    AAVE: strategyAAVE
   },
   ReserveAssets: {
     [eBobNetwork.testnet]: {
       USDC: "0x14E986C4a733B555c317D95Fe0FC5bFB5a7D166C",
       DAI: "0xcb913C75362A7Fd39de6A5DDE4341F370F5B4419",
       WETH: "0x327E7E4A9e054ecC67dFa9E3Af158347116321Bf",
+      AAVE: "0xF1b760dcB43A93694333A0E0ABc20F4D3e611985"
     },
     [eBobNetwork.testnet_old]: {
       USDC: "0x509AeFe02953BC2fB8abCa53Fd83C94D86c05922",
@@ -52,6 +55,7 @@ export const BobConfig: IAaveConfiguration = {
     [eBobNetwork.testnet]: {
       DAI: "0xb062542b2A173fe90E885C1A2bF6C87F842167d0",
       USDC: "0xb062542b2A173fe90E885C1A2bF6C87F842167d0",
+      AAVE: "0x61f72e0419c2D3073bA4A78CB3f2075625Ff6f5B",
       WETH: "0x5281b36049dDdcb2161dACab4ec5e80b638459c6",
     },
     [eBobNetwork.main]: {
